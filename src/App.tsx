91be30b49289994396b7971f246fef0bed036c3b
nc0le <nicolezcui@gmail.com>
2025-07-11 08:48:14 -0400

    analytics

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import profilePic from './assets/websitephoto.jpg'
 import { FaGithub, FaLinkedin } from 'react-icons/fa';
 import { useState } from 'react'
 import TerminalEmulator from './Terminal';
+import { Analytics } from '@vercel/analytics/react';
+
 
 function App() {
   const [showTerminal, setShowTerminal] = useState(false);
@@ -35,86 +37,90 @@ function App() {
           </p>
         </div>
         <TerminalEmulator />
+        <Analytics />
       </div>
     );
   }
 
   return (
     <div>
-      {/* Top-left terminal toggle button */}
-      <button
-        onClick={() => setShowTerminal(true)}
-        style={{
-          position: 'absolute',
-          top: '20px',
-          left: '20px',
-          zIndex: 10,
-          padding: '8px 12px',
-          backgroundColor: 'transparent', 
-          color: '#000',
-          border: '1px solid #000', // Black border
-          borderRadius: '5px',
-          cursor: 'pointer',
-          fontFamily: 'Azeret Mono, monospace',
-          fontSize: '12px',
-          fontWeight: 'normal'
-        }}
-      >
-        Open Terminal
-      </button>
-
       <div>
-        <img 
-          src={profilePic} 
-          alt="Nicole Cui" 
-          style={{ 
-            width: '200px', 
-            height: '200px', 
-            objectFit: 'cover', 
-            borderRadius: '50%',
-            objectPosition: '50% 20%',
-          }}
-        />
-      </div>
-
-      <h1 style={{ fontFamily: 'Azeret Mono, monospace', fontSize: 40 , fontWeight: 'normal', color: '#000000'}}>Nicole Cui</h1>
-      <div className="card">
-        <p>
-        Hello! I'm an undergraduate at Columbia University studying computer science. Check out my terminal for more info!
-        </p>
-        <p>
-          <b>Contact:</b> n.cui@columbia.edu
-        </p>
-        <a 
-          href="https://github.com/nc0le" 
-          target="_blank" 
-          rel="noopener noreferrer"
+        {/* Top-left terminal toggle button */}
+        <button
+          onClick={() => setShowTerminal(true)}
           style={{
-            display: 'inline-block',
-            marginTop: '15px',
-            margin: '10px',
-            fontSize: '35px',
-            color: '#000000'
+            position: 'absolute',
+            top: '20px',
+            left: '20px',
+            zIndex: 10,
+            padding: '8px 12px',
+            backgroundColor: 'transparent', 
+            color: '#000',
+            border: '1px solid #000', // Black border
+            borderRadius: '5px',
+            cursor: 'pointer',
+            fontFamily: 'Azeret Mono, monospace',
+            fontSize: '12px',
+            fontWeight: 'normal'
           }}
         >
-          <FaGithub />
-        </a>
+          Open Terminal
+        </button>
 
-        <a 
-          href="https://www.linkedin.com/in/nicole-cui/" 
-          target="_blank" 
-          rel="noopener noreferrer"
-          style={{
-            display: 'inline-block',
-            margin: '10px',
-            fontSize: '35px',
-            color: '#000000'
-          }}
-        >
-          <FaLinkedin />
-        </a>
-        
+        <div>
+          <img 
+            src={profilePic} 
+            alt="Nicole Cui" 
+            style={{ 
+              width: '200px', 
+              height: '200px', 
+              objectFit: 'cover', 
+              borderRadius: '50%',
+              objectPosition: '50% 20%',
+            }}
+          />
+        </div>
+
+        <h1 style={{ fontFamily: 'Azeret Mono, monospace', fontSize: 40 , fontWeight: 'normal', color: '#000000'}}>Nicole Cui</h1>
+        <div className="card">
+          <p>
+          Hello! I'm an undergraduate at Columbia University studying computer science. Check out my terminal for more info!
+          </p>
+          <p>
+            <b>Contact:</b> n.cui@columbia.edu
+          </p>
+          <a 
+            href="https://github.com/nc0le" 
+            target="_blank" 
+            rel="noopener noreferrer"
+            style={{
+              display: 'inline-block',
+              marginTop: '15px',
+              margin: '10px',
+              fontSize: '35px',
+              color: '#000000'
+            }}
+          >
+            <FaGithub />
+          </a>
+
+          <a 
+            href="https://www.linkedin.com/in/nicole-cui/" 
+            target="_blank" 
+            rel="noopener noreferrer"
+            style={{
+              display: 'inline-block',
+              margin: '10px',
+              fontSize: '35px',
+              color: '#000000'
+            }}
+          >
+            <FaLinkedin />
+          </a>
+          
+        </div>
       </div>
+      <Analytics />
     </div>
   )
 }
